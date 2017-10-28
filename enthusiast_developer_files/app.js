a42eec68adb48f39273027ca27396dc90fb1e385
shideneyu <shideneyu@gmail.com>
2017-10-28 13:50:15 +0200

    Feature: Refactored tree and cleaned things

--- enthusiast_developer_files/app.js
+++ enthusiast_developer_files/app.js
@@ -1,5 +1,25 @@
 var app = angular.module('delegateApp', []);
 
+function parseTable(order, ascdesc) {
+    setTimeout(function () {
+     $(".username").each(function(index, item) {
+        value = $(item).text();
+        item.innerHTML = "<a href='delegates.html?username=" + value +"'>"+ value +"</a>";
+     });
+
+    $('table#dataTables-example').dataTable(
+            {
+                aaSorting: [[order, ascdesc]],
+                "drawCallback": function( settings ) {
+                    $('#VotesTable').show('slow');
+                    $('#spinner').hide('slow')
+                }
+            }
+        );    
+    }, 3000);
+}
+
+
 app.controller('indexCtrl', function($scope, $http) {
     $scope.accounts = [];
     $scope.lastpayout = 0;
@@ -11,34 +31,28 @@ app.controller('indexCtrl', function($scope, $http) {
     else {
         var user = window.location.search.substr(10);
     }
-    $http.get ('https://wallet.shiftnrg.org/api/delegates/get?username=' + user).then (function (res) {
-        $scope.delegate = res.data.delegate;
 
-        $http.get('https://wallet.shiftnrg.org/api/delegates/voters?publicKey=' + res.data.delegate.publicKey).then (function (res) {
-           $scope.voters = res.data.accounts;
 
-           setTimeout(function () {
-             $(".username").each(function(index, item) {
-                value = $(item).text();
-                item.innerHTML = "<a href='index.html?username=" + value +"'>"+ value +"</a>";
-             });
-            
-            $('table#dataTables-example').dataTable(
-                    {
-                        aaSorting: [[3, 'desc']],
-                        "drawCallback": function( settings ) {
-                            $('#VotesTable').show('slow');
-                            $('#spinner').hide('slow')
-                        }
-                    }
-                );
-                
-            }, 3000);
+    // if route is delegates show
+    if ((window.location.href.indexOf("/delegates") > -1) || (window.location.href.indexOf("/pool") > -1)) {
+        $http.get ('https://wallet.shiftnrg.org/api/delegates/get?username=' + user).then (function (res) {
+            $scope.delegate = res.data.delegate;
+
+            $http.get('https://wallet.shiftnrg.org/api/delegates/voters?publicKey=' + res.data.delegate.publicKey).then (function (res) {
+               $scope.voters = res.data.accounts;
+
+                parseTable(3, 'desc');
+
+            });
         });
-    });
-
+    }
+    // if route is index
     $http.get('https://wallet.shiftnrg.org/api/delegates?limit=101').then (function (res) {
         $scope.delegates = res.data.delegates;
+        
+        if ((window.location.href.split('/').pop() === "") || (window.location.href.split('/').pop() === "index.html") ) {
+            parseTable(6, 'asc');
+        }
     });
 
 });

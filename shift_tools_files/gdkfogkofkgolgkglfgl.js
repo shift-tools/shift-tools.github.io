128d0b68a2ddf0c117e78a44f275289c50d89b3f
shideneyu <shideneyu@gmail.com>
2017-11-19 19:18:57 +0100

    Feature: Finished Lottery

--- shift_tools_files/gdkfogkofkgolgkglfgl.js
+++ shift_tools_files/gdkfogkofkgolgkglfgl.js
@@ -21,10 +21,12 @@ function parseTable(order, ascdesc) {
 
 
 app.controller('indexCtrl', function($scope, $http) {
-    $scope.accounts = [];
-    $scope.lastpayout = 0;
-    $scope.nextpayout = 0;
-
+    $scope.accounts 			  = [];
+    $scope.lastpayout 			  = 0;
+    $scope.nextpayout 			  = 0;
+    current_ticket_number_index   = 0;
+    ticketRange                   = {};
+    previous_ticket_number        = -1;
     if (window.location.search.substr(10) == '') {
         var user = 'shift_tools';
     }
@@ -40,6 +42,35 @@ app.controller('indexCtrl', function($scope, $http) {
 	        $http.get('http://51.255.196.160:8000/data.json').then (function (res) {
 	            $scope.participants = res.data;
         	});
+
+			$scope.getPercentage = function(percentage) {
+			   return percentage.toFixed(2);
+			}
+
+			$scope.getTicketsRange = function(percentage, index, last_voter_index) {
+				if (index > current_ticket_number_index) {
+					console.log(index);
+					number_of_tickets  = (percentage/100)*10000;
+					if (number_of_tickets >= 1) {
+						first_ticket_index = previous_ticket_number + 1;
+						last_ticket_index  = first_ticket_index + number_of_tickets - 1;
+					}
+					else {
+						first_ticket_index = 0;
+						last_ticket_index  = -1;
+					}
+				    previous_ticket_number = last_ticket_index;
+
+				    if (index+1 == last_voter_index) {
+				    	last_ticket_index = '9999';
+				    }
+				    ticketRange[index] = [parseInt(first_ticket_index), parseInt(last_ticket_index)]
+				    current_ticket_number_index = current_ticket_number_index + 1;
+
+				}
+
+				return ('From ' + ticketRange[index][0] + ' to ' + ticketRange[index][1]);
+			}
     	}
 
         $http.get ('https://wallet.shiftnrg.org/api/delegates/get?username=' + user).then (function (res) {
@@ -71,7 +102,6 @@ app.controller('indexCtrl', function($scope, $http) {
             }, 4000);
 
             const loop = [...Array(8)].map((_, i) => {
-                console.log(i);
               $http.get('https://wallet.shiftnrg.org/api/delegates?offset='+parseInt(i+1)+'01').then (function (res) {
                 $scope.delegates = res.data.delegates.concat($scope.delegates);
               });
